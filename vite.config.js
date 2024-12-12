2b130458f776a9f6b2c4425221107c56c8a3fe9d
Jimmyis <jimmyis.engineerings@gmail.com>
2024-12-12 23:09:44 +0700

    Added /dist folder for static web access (GitHub Pages)

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,23 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { resolve } from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/fart-counter/", // Base URL for the app
+  server: {
+    port: 3000 // Development server port
+  },
   plugins: [vue()],
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, 'src') // Alias for src directory
+    }
+  },
+  build: {
+    outDir: 'dist', // Output directory
+    rollupOptions: {
+      input: resolve(__dirname, 'index.html') // Entry point for the build
+    }
+  }
 })
